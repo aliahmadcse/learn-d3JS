7e720a26334314a78f55ff1f5a4e460ca1186141
aliahmadcse <aliahmadcse@gmail.com>
2020-07-11 15:37:23 +0500

    Configure app

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <h2>Name:{{ name }}</h2>
+    </div>
 </template>
+
+
+<script>
+export default {
+    computed: {
+        name() {
+            return this.$store.state.name;
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
