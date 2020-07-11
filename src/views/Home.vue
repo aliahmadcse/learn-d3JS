919dc8050492522fd4fda201c920091d98cb1d85
aliahmadcse <aliahmadcse@gmail.com>
2020-07-11 17:18:07 +0500

    Adds first method of d3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,31 @@
 <template>
-    <div class="home">
-        <h1>Home View</h1>
+    <div>
+        <h1 class="text-center">Home View</h1>
+        <div class="d3"></div>
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
-
+import * as d3 from 'd3';
 export default {
     name: 'Home',
     data() {
         return {};
     },
+
     mounted() {
-        
+        this.appendElement();
     },
-    computed: {},
-    methods: {}
+
+    methods: {
+        appendElement() {
+            d3.select('.d3')
+                .append('h1')
+                .text('Learning D3');
+        }
+    }
 };
 </script>
 
