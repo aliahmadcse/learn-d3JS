305735fef7b8b40fa5a85ac65392f24005285cc3
aliahmadcse <aliahmadcse@gmail.com>
2020-07-13 19:13:06 +0500

    Adds linear scales

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,14 +24,25 @@ export default {
                 [78, 320],
                 [21, 123]
             ],
-
+            linearDataSet: [
+                [34, 78],
+                [109, 280],
+                [310, 120],
+                [79, 411],
+                [420, 220],
+                [233, 145],
+                [333, 96],
+                [222, 333],
+                [78, 320],
+                [21, 123]
+            ],
             h: 200,
             w: 500
         };
     },
 
     mounted() {
-        this.addCircle();
+        this.linearScale();
     },
 
     methods: {
@@ -95,6 +106,55 @@ export default {
                 .attr('x', d => d[0] + 5)
                 .attr('y', d => 500 - d[1])
                 .text(d => `${d[0]}, ${d[1]}`);
+        },
+        linearScale() {
+            const w = 500;
+            const h = 500;
+            // Padding between the SVG canvas boundary and the plot
+            const padding = 30;
+
+            const xScale = d3
+                .scaleLinear()
+                .domain([0, d3.max(this.linearDataSet, d => d[0])])
+                .range([padding, w - padding]);
+
+            const yScale = d3
+                .scaleLinear()
+                .domain([0, d3.max(this.linearDataSet, d => d[1])])
+                .range([h - padding, padding]);
+
+            const svg = d3
+                .select('body')
+                .append('svg')
+                .attr('width', w)
+                .attr('height', h);
+
+            svg.selectAll('circle')
+                .data(this.linearDataSet)
+                .enter()
+                .append('circle')
+                .attr('cx', d => xScale(d[0]))
+                .attr('cy', d => yScale(d[1]))
+                .attr('r', 5);
+
+            svg.selectAll('text')
+                .data(this.linearDataSet)
+                .enter()
+                .append('text')
+                .text(d => d[0] + ', ' + d[1])
+                .attr('x', d => xScale(d[0] + 10))
+                .attr('y', d => yScale(d[1]));
+
+            const xAxis = d3.axisBottom(xScale);
+            const yAxis = d3.axisLeft(yScale);
+
+            svg.append('g')
+                .attr('transform', `translate(0,${h - padding})`)
+                .call(xAxis);
+
+            svg.append('g')
+                .attr('transform', `translate(${padding},0)`)
+                .call(yAxis);
         }
     }
 };
