3e46bf71da547a14ebdd1ea22bf23e370e308124
aliahmadcse <aliahmadcse@gmail.com>
2020-07-13 15:22:01 +0500

    📦 NEW: Svg circle

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,13 +12,26 @@ export default {
     data() {
         return {
             dataSet: [12, 31, 22, 17, 25, 18, 29, 14, 9],
+            circleDataSet: [
+                [34, 78],
+                [109, 280],
+                [310, 120],
+                [79, 411],
+                [420, 220],
+                [233, 145],
+                [333, 96],
+                [222, 333],
+                [78, 320],
+                [21, 123]
+            ],
+
             h: 200,
             w: 500
         };
     },
 
     mounted() {
-        this.addSvg();
+        this.addCircle();
     },
 
     methods: {
@@ -46,7 +59,10 @@ export default {
                 .attr('y', d => this.h - d * 3)
                 .attr('width', 25)
                 .attr('height', d => d * 3)
-                .attr('fill', 'navy');
+                .attr('fill', 'navy')
+                .attr('class', 'bar')
+                .append('title')
+                .text(d => d);
 
             svg.selectAll('text')
                 .data(this.dataSet)
@@ -55,7 +71,30 @@ export default {
                 .attr('x', (d, i) => i * 35)
                 .attr('y', d => this.h - d * 3 - 5)
                 .text(d => d)
-                .attr('fill','red');
+                .attr('fill', 'red');
+        },
+        addCircle() {
+            const svg = d3
+                .select('#d3')
+                .append('svg')
+                .attr('width', 500)
+                .attr('height', 500);
+
+            svg.selectAll('circle')
+                .data(this.circleDataSet)
+                .enter()
+                .append('circle')
+                .attr('cx', d => d[0])
+                .attr('cy', d => 500 - d[1])
+                .attr('r', 5);
+
+            svg.selectAll('text')
+                .data(this.circleDataSet)
+                .enter()
+                .append('text')
+                .attr('x', d => d[0] + 5)
+                .attr('y', d => 500 - d[1])
+                .text(d => `${d[0]}, ${d[1]}`);
         }
     }
 };
@@ -68,5 +107,8 @@ export default {
     margin: 2px;
     display: inline-block;
     background-color: blue;
+}
+.bar:hover {
+    fill: blueviolet;
 }
 </style>
