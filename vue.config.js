a57fd2e05f7cc57d31f97f7abccf185e43871e61
aliahmadcse <aliahmadcse@gmail.com>
2020-07-14 22:42:30 +0500

    Adds vue config comment

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
     css: {
         loaderOptions: {
             sass: {
+                // @ is an alias for /src
                 prependData: `@import "@/sass/_variables.scss";`
             }
         }
